0b9c3e9dfdd369ce1427732c3c3eb604c2394922
재용 <jaeyong@gangjingyeong-ui-MacBook-Pro.local>
2023-10-26 17:37:23 +0900

    feat. categories, products, header UI.

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,8 +1,53 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.render('products');
+const { getAllCategories } = require('../middleware/product');
+
+const Product = require('../models/products.model');
+
+const fs = require('fs-extra');
+
+router.get('/', getAllCategories, async (req, res, next) => {
+  try {
+    const products = await Product.find({});
+    res.render('products', {
+      products,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get('/:category', getAllCategories, async (req, res, next) => {
+  try {
+    const categorySlug = req.params.category;
+
+    const products = await Product.find({ category: categorySlug });
+    res.render('products', {
+      products,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get('/:category/:product', async (req, res, next) => {
+  try {
+    const product = await Product.findOne({ slug: req.params.product });
+    const galleryDirectory =
+      'src/public/product-images/' + product._id + '/gallery';
+    const galleryImages = await fs.readdir(galleryDirectory);
+
+    res.render('product', {
+      product,
+      galleryImages,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 module.exports = router;
